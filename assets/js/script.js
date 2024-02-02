ded6d04457b8ecabb4213717fb796a37e0b3264c
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-02-03 00:21:34 +0500

    loading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,80 +1,97 @@
-/**
- * Fetches benchmark results from 'benchmark_output.json' and displays them on the page.
- * Also includes a bar chart visualizing rendering times and information about the fastest framework.
- * @function
- * @returns {void}
- */
-function loadAndDisplayResults() {
-    fetch("benchmark_output.json")
-        .then((response) => response.json())
-        .then((data) => {
-            const resultsDiv = document.getElementById("results");
-            const lastUpdatedDiv = document.getElementById("last-updated");
-            const chartContainer = document.getElementById("chart-container");
-            const fastestFrameworkDiv =
-                document.getElementById("fastest-framework");
+document.addEventListener('DOMContentLoaded', function () {
+    function showLoading() {
+        const loadingDiv = document.getElementById('loading');
+        loadingDiv.classList.remove('hidden');
+    }
 
-            resultsDiv.innerHTML = ""; // Clear existing results
-            fastestFrameworkDiv.innerHTML = "";
+    function hideLoading() {
+        const loadingDiv = document.getElementById('loading');
+        loadingDiv.classList.add('hidden');
+    }
 
-            const results = data.results;
+    /**
+     * Fetches benchmark results from 'benchmark_output.json' and displays them on the page.
+     * Also includes a bar chart visualizing rendering times and information about the fastest framework.
+     * @function
+     * @returns {void}
+     */
+    function loadAndDisplayResults() {
+        showLoading()
 
-            data.results.forEach((result) => {
-                const resultElement = document.createElement("div");
-                resultElement.className = "result";
-                resultElement.textContent = `${result.name}: ${result.time} seconds`;
-                resultsDiv.appendChild(resultElement);
-            });
+        fetch("benchmark_output.json")
+            .then((response) => response.json())
+            .then((data) => {
+                hideLoading()
 
-            // Display Last Updated At
-            lastUpdatedDiv.textContent = `Last Updated At: ${data.lastUpdated}`;
+                const resultsDiv = document.getElementById("results");
+                const lastUpdatedDiv = document.getElementById("last-updated");
+                const chartContainer = document.getElementById("chart-container");
+                const fastestFrameworkDiv =
+                    document.getElementById("fastest-framework");
 
-            const chartData = {
-                labels: results.map((result) => result.name),
-                datasets: [
+                resultsDiv.innerHTML = ""; // Clear existing results
+                fastestFrameworkDiv.innerHTML = "";
+
+                const results = data.results;
+
+                data.results.forEach((result) => {
+                    const resultElement = document.createElement("div");
+                    resultElement.className = "result";
+                    resultElement.textContent = `${result.name}: ${result.time} seconds`;
+                    resultsDiv.appendChild(resultElement);
+                });
+
+                // Display Last Updated At
+                lastUpdatedDiv.textContent = `Last Updated At: ${data.lastUpdated}`;
+
+                const chartData = {
+                    labels: results.map((result) => result.name),
+                    datasets: [
+                        {
+                            label: "Rendering Time (seconds)",
+                            backgroundColor: "rgba(75, 192, 192, 0.2)",
+                            borderColor: "rgba(75, 192, 192, 1)",
+                            borderWidth: 1,
+                            data: results.map((result) => parseFloat(result.time)),
+                        },
+                    ],
+                };
+
+                const chartOptions = {
+                    scales: {
+                        y: {
+                            beginAtZero: true,
+                        },
+                    },
+                };
+
+                const benchmarkChart = new Chart(
+                    document.getElementById("benchmark-chart"),
                     {
-                        label: "Rendering Time (seconds)",
-                        backgroundColor: "rgba(75, 192, 192, 0.2)",
-                        borderColor: "rgba(75, 192, 192, 1)",
-                        borderWidth: 1,
-                        data: results.map((result) => parseFloat(result.time)),
-                    },
-                ],
-            };
+                        type: "bar",
+                        data: chartData,
+                        options: chartOptions,
+                    }
+                );
 
-            const chartOptions = {
-                scales: {
-                    y: {
-                        beginAtZero: true,
-                    },
-                },
-            };
+                const fastestFramework = results.reduce((fastest, current) => {
+                    return parseFloat(current.time) < parseFloat(fastest.time)
+                        ? current
+                        : fastest;
+                }, results[0]);
 
-            const benchmarkChart = new Chart(
-                document.getElementById("benchmark-chart"),
-                {
-                    type: "bar",
-                    data: chartData,
-                    options: chartOptions,
-                }
-            );
+                // Display information about the fastest framework
+                fastestFrameworkDiv.textContent = `Fastest Framework: ${fastestFramework.name} (${fastestFramework.time} seconds)`;
+            })
+            .catch((error) => {
+                hideLoading()
+                console.error("Error loading benchmark results:", error)
+            });
+    }
 
-            const fastestFramework = results.reduce((fastest, current) => {
-                return parseFloat(current.time) < parseFloat(fastest.time)
-                    ? current
-                    : fastest;
-            }, results[0]);
+    // Load and display results initially
+    loadAndDisplayResults();
 
-            // Display information about the fastest framework
-            fastestFrameworkDiv.textContent = `Fastest Framework: ${fastestFramework.name} (${fastestFramework.time} seconds)`;
-        })
-        .catch((error) =>
-            console.error("Error loading benchmark results:", error)
-        );
-}
-
-// Load and display results initially
-loadAndDisplayResults();
-
-// Refresh results every 1 minute
-setInterval(loadAndDisplayResults, 1 * 60 * 1000);
+    // Refresh results every 1 minute
+    setInterval(loadAndDisplayResults, 1 * 60 * 1000);
+})
