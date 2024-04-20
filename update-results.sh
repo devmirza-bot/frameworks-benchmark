754d6625d09e8a09ebdea97fc97f1b32039abd66
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-04-20 10:00:43 -0700

    Typo

--- update-results.sh
+++ update-results.sh
@@ -5,5 +5,5 @@ node benchmark.js
 git config --global user.name 'devmirza-bot'
 git config --global user.email 'mirzazaid4740@gmail.com'
 git add .
-git commit -m ':rocket: Update Benchmark Results & Screenshot'
+git commit -m ':rocket: Update Benchmark Results'
 git push
