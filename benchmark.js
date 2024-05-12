5958280b07c357bf7eacc03674c53eafe1819be9
DevMirza <pzhafeez@gmail.com>
2024-05-12 08:28:20 +0000

    Add nextjs 13

--- benchmark.js
+++ benchmark.js
@@ -1,6 +1,6 @@
-const React = require('react');
-const Vue = require('vue');
-const fs = require('fs');
+const React = require("react");
+const Vue = require("vue");
+const fs = require("fs");
 
 /**
  * Represents a framework used in the benchmark.
@@ -10,24 +10,46 @@ const fs = require('fs');
  * @property {function} render - The rendering function that takes data and returns the rendered output.
  */
 
-
 const frameworks = [
-    {
-        name: 'React 18',
-        library: React,
-        render: (data) => data.map((item) => React.createElement('div', { key: item.id }, item.name)),
-    },
-    {
-        name: 'Next.js 14',
-        library: require('next'),
-        render: (data) => React.createElement('div', null, data.map((item) => React.createElement('div', { key: item.id }, item.name))),
-    },
-    {
-        name: 'Vue 3',
-        library: Vue,
-        render: (data) => data.map((item) => Vue.h('div', { key: item.id }, item.id))
-    },
-    // Add other frameworks here
+  {
+    name: "React 18",
+    library: React,
+    render: (data) =>
+      data.map((item) =>
+        React.createElement("div", { key: item.id }, item.name)
+      ),
+  },
+  {
+    name: "Next.js 14",
+    library: require("next"),
+    render: (data) =>
+      React.createElement(
+        "div",
+        null,
+        data.map((item) =>
+          React.createElement("div", { key: item.id }, item.name)
+        )
+      ),
+  },
+  {
+    name: "Next.js 13",
+    library: require("next-v13"),
+    render: (data) =>
+      React.createElement(
+        "div",
+        null,
+        data.map((item) =>
+          React.createElement("div", { key: item.id }, item.name)
+        )
+      ),
+  },
+  {
+    name: "Vue 3",
+    library: Vue,
+    render: (data) =>
+      data.map((item) => Vue.h("div", { key: item.id }, item.id)),
+  },
+  // Add other frameworks here
 ];
 
 /**
@@ -37,7 +59,7 @@ const frameworks = [
  * @returns {Array<Object>} - An array of sample data objects.
  */
 const generateSampleData = (length) =>
-    Array.from({ length }, (_, index) => ({ id: index, name: `Item ${index}` }));
+  Array.from({ length }, (_, index) => ({ id: index, name: `Item ${index}` }));
 
 /**
  * Save results to a historical log file
@@ -45,20 +67,25 @@ const generateSampleData = (length) =>
  * @param {Date} timestamp - Timestamp of the benchmark run
  */
 function saveToHistory(results, timestamp) {
-    try {
-        let historicalData = [];
+  try {
+    let historicalData = [];
 
-        // Check if the file exists
-        if (fs.existsSync('historical_log.json')) {
-            historicalData = JSON.parse(fs.readFileSync('historical_log.json', 'utf-8'));
-        }
-
-        historicalData.push({ results, timestamp });
-        fs.writeFileSync('historical_log.json', JSON.stringify(historicalData, null, 2));
-        console.log('Results saved to historical log.');
-    } catch (error) {
-        console.error('Error saving results to historical log:', error);
+    // Check if the file exists
+    if (fs.existsSync("historical_log.json")) {
+      historicalData = JSON.parse(
+        fs.readFileSync("historical_log.json", "utf-8")
+      );
     }
+
+    historicalData.push({ results, timestamp });
+    fs.writeFileSync(
+      "historical_log.json",
+      JSON.stringify(historicalData, null, 2)
+    );
+    console.log("Results saved to historical log.");
+  } catch (error) {
+    console.error("Error saving results to historical log:", error);
+  }
 }
 
 /**
@@ -67,56 +94,60 @@ function saveToHistory(results, timestamp) {
  * @returns {void}
  */
 const runBenchmark = () => {
-    const data = generateSampleData(1000);
-    const results = [];
+  const data = generateSampleData(1000);
+  const results = [];
 
-    frameworks.forEach((framework) => {
-        /**
-         * Represents the result of rendering using a specific framework.
-         * @typedef {Object} BenchmarkResult
-         * @property {string} name - The name of the framework.
-         * @property {string} time - The rendering time in seconds.
-         */
-
-        /** @type {BenchmarkResult} */
-        const result = {
-            name: framework.name,
-            time: undefined,
-        };
-
-        const startTime = performance.now();
-        const renderedOutput = framework.render(data);
-        const endTime = performance.now();
-        const elapsedTimeInSeconds = (endTime - startTime) / 1000;
-
-        result.time = elapsedTimeInSeconds.toFixed(4);
-        results.push(result);
-
-        console.log(`${framework.name} took ${elapsedTimeInSeconds.toFixed(4)} seconds to render.`);
-    });
-
-    const lastUpdated = new Date()
-
-    const timestamp = new Date()
-
-    saveToHistory(results, timestamp)
-
+  frameworks.forEach((framework) => {
     /**
-     * Represents the overall benchmark results.
-     * @typedef {Object} BenchmarkResults
-     * @property {Array<BenchmarkResult>} results - An array of results for each framework.
-     * @property {string} lastUpdated - The timestamp when the benchmark was last updated.
+     * Represents the result of rendering using a specific framework.
+     * @typedef {Object} BenchmarkResult
+     * @property {string} name - The name of the framework.
+     * @property {string} time - The rendering time in seconds.
      */
 
-    /** @type {BenchmarkResults} */
-    const benchmarkResults = {
-        results,
-        lastUpdated: lastUpdated.toLocaleString(),
+    /** @type {BenchmarkResult} */
+    const result = {
+      name: framework.name,
+      time: undefined,
     };
 
-    const jsonResults = JSON.stringify(benchmarkResults, null, 2);
+    const startTime = performance.now();
+    const renderedOutput = framework.render(data);
+    const endTime = performance.now();
+    const elapsedTimeInSeconds = (endTime - startTime) / 1000;
 
-    fs.writeFileSync('benchmark_output.json', jsonResults);
+    result.time = elapsedTimeInSeconds.toFixed(4);
+    results.push(result);
+
+    console.log(
+      `${framework.name} took ${elapsedTimeInSeconds.toFixed(
+        4
+      )} seconds to render.`
+    );
+  });
+
+  const lastUpdated = new Date();
+
+  const timestamp = new Date();
+
+  saveToHistory(results, timestamp);
+
+  /**
+   * Represents the overall benchmark results.
+   * @typedef {Object} BenchmarkResults
+   * @property {Array<BenchmarkResult>} results - An array of results for each framework.
+   * @property {string} lastUpdated - The timestamp when the benchmark was last updated.
+   */
+
+  /** @type {BenchmarkResults} */
+  const benchmarkResults = {
+    results,
+    lastUpdated: lastUpdated.toLocaleString(),
+  };
+
+  const jsonResults = JSON.stringify(benchmarkResults, null, 2);
+
+  fs.writeFileSync("benchmark_output.json", jsonResults);
 };
 
 // Run the benchmark initially
